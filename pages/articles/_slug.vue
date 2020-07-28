0c0bf9b6955b755bc544a1c2a097d922f23ac92f
Sébastien Chopin <seb@nuxtjs.com>
2020-07-28 17:47:04 +0200

    chore: handle 404 on article

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
+  <div v-if="article">
     <h3>{{ article.title }}</h3>
     <img :src="article.imageUrl" />
     <p>{{ article.body }}</p>
+  </div>
+  <div v-else>
+    <h3>Article not found</h3>
+    <NuxtLink to="/articles">Back to articles</NuxtLink>
   </div>
 </template>
 
 <script>
 export default {
   async asyncData({ $strapi, params }) {
-    const article = await $strapi.$articles.findOne(params.slug)
+    const article = await $strapi.$articles.findOne(params.slug).catch(e => null)
 
     return { article }
   }
