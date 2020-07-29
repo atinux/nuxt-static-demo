94ac3f7bd70618ce7133b5607e87a68632d6a0e5
Sébastien Chopin <seb@nuxtjs.com>
2020-07-29 11:48:44 +0200

    chore: add meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,10 @@
 <script>
 export default {
   head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
     link: [
       { rel: 'stylesheet', href: 'https://fonts.xz.style/serve/inter.css' },
       { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css' }
